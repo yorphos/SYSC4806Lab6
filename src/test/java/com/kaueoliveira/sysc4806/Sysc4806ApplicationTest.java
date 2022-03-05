e88029b4f4f93f845d85c9941f73151e1e11a159
Kaue Oliveira <elmovckaue@gmail.com>
2022-03-04 21:05:14 -0500

    Fix and update tests

---
+++ src/test/java/com/kaueoliveira/sysc4806/Sysc4806ApplicationTest.java
@@ -0,0 +1,87 @@
+package com.kaueoliveira.sysc4806;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class Sysc4806ApplicationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    private String toJson(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch(JsonProcessingException e) {
+            return "";
+        }
+    }
+
+    @Test
+    public void testCreateBook() throws Exception {
+        mvc.perform(post("/createbook").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(toJson(new AddressBook(1)))));
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+        mvc.perform(post("/createbook"));
+        mvc.perform(post("/deletebook?bookId=1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetBuddies() throws Exception {
+        mvc.perform(post("/createbook"));
+        mvc.perform(post("/addbuddy?bookId=1").contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(new BuddyInfo("test", "test"))));
+        mvc.perform(get("/getbuddies?bookId=1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(equalTo(toJson(new ArrayList<BuddyInfo>() {{
+                    add(new BuddyInfo("test", "test", 1));
+                }}))));
+    }
+
+    @Test
+    public void testAddBuddy() throws Exception {
+        mvc.perform(post("/createbook"));
+        mvc.perform(post("/addbuddy?bookId=1").contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(new BuddyInfo("test", "test"))))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(toJson(new AddressBook(1) {{
+                    addBuddy(new BuddyInfo("test", "test", 1));
+                }}))));
+    }
+
+    @Test
+    public void testRemoveBuddy() throws Exception {
+        mvc.perform(post("/createbook"));
+        mvc.perform(post("/addbuddy?bookId=1").contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(new BuddyInfo("test", "test"))));
+        mvc.perform(post("/addbuddy?bookId=1").contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(new BuddyInfo("test1", "test1"))));
+        mvc.perform(post("/deletebuddy?bookId=1").contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(new BuddyInfo("test", "test")))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(toJson(new AddressBook(1) {{
+                    addBuddy(new BuddyInfo("test1", "test1", 2));
+                }}))));
+    }
+}
